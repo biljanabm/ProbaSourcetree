156334b8d2759e2503cfcde3246deeaa9fe61d6e
biljanabm <bmgrafika@yahoo.com>
2023-05-06 15:28:59 +0200

    Proba Sourcefree programa

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { initApi } from "./api/initApi";                
+import { dbConnection } from "./mysql/initMysql";       
+
+
+initApi();                      
+dbConnection.connect();         
+
+// ovo sve pisemo za mySQL
+//ovo je proba Sourcetree programa
